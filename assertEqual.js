70c3f2f856095444c063d3a121e11cc0868998e1
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-06 18:36:25 +0000

    Created a function which will output whether two passed variables are equal/===

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\u2714 \u2714 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("\u2716 \u2716 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Job", "Job");
+assertEqual(1,1);
+assertEqual(4, 7);
