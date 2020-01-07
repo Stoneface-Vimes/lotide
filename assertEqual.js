2b77252379edf8f192413e7e38cbf5e7251465c6
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-07 23:43:25 +0000

    Added a condition to check for empty arrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\u2714 \u2714 Assertion Passed: " + actual + " === " + expected);
+    console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("\u2716 \u2716 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`\u2716 \u2716 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
