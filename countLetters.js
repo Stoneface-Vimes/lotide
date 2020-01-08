adb18fdb5f12d63957efa4d95edd0613bae59471
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-08 18:25:14 +0000

    created a function that counts how many times letters occur in a string. returns an object, where keys = letter & values = # of times letter occurs

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u2716 \u2716 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let count = {} //Keys = letter in str : Values = number of times keys appears in string
+  for (element of str) {
+    if (count[element]){
+      count[element] += 1;
+    } else {
+      count[element] = 1;
+    }
+  }
+  return count;
+}
+
+const test = 'abcaabbcc';
+
+console.log(countLetters(test));
