cc95938b579ad20f14ed0fb4635efcb13fa9eb55
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-10 17:40:31 -0500

    Created a function that, when given an object and a callback function, returns the key that corresponds to the callback function.

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u2716 \u2716 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+
+  for (let x in obj) {
+    if (callback(obj[x])){
+      return x;  
+    }
+  }
+
+}
+
+const x = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(x, 'noma');
