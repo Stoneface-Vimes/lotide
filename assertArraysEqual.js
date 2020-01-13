e9c24fc01e1b7384ff88877d6b7c2af0bcff0dba
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-13 14:23:13 -0500

    Updated with a more inclusive assertArrays equal function, eqArrays is not reqruied to run but is imported anyway

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,10 +2,14 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
 
-  if (actual.length === expected.length){
+  if (actual.length === 0 && expected.length === 0){ //Checks if both parameters are empty arrays
+    console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
+
+  } else if (actual.length === expected.length){
     for (let i = 0; i < actual.length; i++){
       if (actual[i] !== expected[i]) {
         console.log(`\u2716 \u2716 Assertion Failed: ${actual} !== ${expected}`);
+        break;
       } else if (i === actual.length - 1){
         console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
       }
