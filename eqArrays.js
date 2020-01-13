bb6e7f9ab929685046c775052254db8cf6e1259e
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-13 15:28:57 -0500

    Removed the test cases, removed function for assertEqual and imported it instead, added export for the eqArrays Function

--- eqArrays.js
+++ eqArrays.js
@@ -20,12 +20,4 @@ const eqArrays = function(actual, expected) {
   }
 };
 
-// TEST CODE
-// assertEqual(eqArrays([], []), true);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-// assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']),true);
-// assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
-
 module.exports = eqArrays;
