966a4499d84c6c6e6aba0116a8b5fef2c8796e11
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-13 16:14:37 -0500

    refactored middleTest to mocha/chai syntax

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,31 @@
-
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []); //True
-assertArraysEqual(middle([1, 2]), [3]); //False
-assertArraysEqual(middle([1, 2, 3]), [2]); //True
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [1]); //False
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); //True
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [1,4]); //False
+describe('#middle', () => {
+  it('returns true when checking [1] against []', () => {
+    assert.deepEqual(middle([1]), []);
+  })
+  it('returns false when checking [1, 2] against []', () => {
+    assert.notDeepEqual(middle([1],2), [3]);
+  })
+  it('returns true when checking [1, 2, 3] against [2]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  })
+  it('returns false when checking [1, 2, 3, 4, 5] against [1]', () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5]), [1]);
+  })
+  it('returns true when checking [1, 2, 3, 4] against [2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+  it('returns false when checking [1, 2, 3, 4, 5, 6] against [1, 4]', () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4, 5, 6,]), [1, 4]);
+  })
+})
+
+
+// assertArraysEqual(middle([1]), []); //True
+// assertArraysEqual(middle([1, 2]), [3]); //False
+// assertArraysEqual(middle([1, 2, 3]), [2]); //True
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [1]); //False
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); //True
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [1,4]); //False
