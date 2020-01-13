b605377e0427bb4654458890c54553a5b02ca9a8
Stoneface-Vimes <cliffweir92@gmail.com>
2020-01-13 13:52:29 -0500

    refactored eqArrays into an exported funtion. Created a test function for eqArrays. Worked with @user-test123 & @drpaddle

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u2716 \u2716 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(actual, expected) {
 
@@ -26,9 +21,11 @@ const eqArrays = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays([], []), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']),true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+// assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']),true);
+// assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+
+module.exports = eqArrays;
